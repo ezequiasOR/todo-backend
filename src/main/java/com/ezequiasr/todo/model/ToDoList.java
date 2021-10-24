9c0de0ffc5c072fb131035949701a1d747005d47
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-10-24 16:52:06 -0300

    #5 - fix

--- src/main/java/com/ezequiasr/todo/model/ToDoList.java
+++ src/main/java/com/ezequiasr/todo/model/ToDoList.java
@@ -1,6 +1,10 @@
 package com.ezequiasr.todo.model;
 
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class ToDoList {
